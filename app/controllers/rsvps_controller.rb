cee65ff462e91845bdb6d979334bda098c33a950
aemassa <annaemassa@gmail.com>
2015-08-06 18:28:29 -0400

    Creates a controller for RSVPs.

---
+++ app/controllers/rsvps_controller.rb
@@ -0,0 +1,42 @@
+class RsvpsController < ApplicationController
+  #skip_before_action :authenticate, only: :index
+
+  def index
+    render json: Rsvp.all
+    #render json: current_user.events
+  end
+
+  def show
+    render json: Rsvp.find(params[:id])
+  end
+
+  def create
+    rsvp = Rsvp.create(rsvp_params)
+    if rsvp.save
+      render json: rsvp
+    else
+      render json: rsvp.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    rsvp = Rsvp.find(params[:id])
+    if rsvp.update(rsvp_params)
+      rsvp.save
+      render json: rsvp
+    else
+      render json: rsvp.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    rsvp = Rsvp.find(params[:id])
+    rsvp.destroy!
+    head :ok
+  end
+
+  private
+  def rsvp_params
+    params.require(:rsvp).permit(:confirmed, :item, :user_id, :event_id)
+  end
+end
