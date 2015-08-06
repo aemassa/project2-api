469ba06a3c9c379d4ce1c8e7047cb13fa12a1203
aemassa <annaemassa@gmail.com>
2015-08-06 18:24:07 -0400

    Adds a route for RSVPs.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   post '/login/' => 'users#login'
   patch '/events/:id' => 'events#update'
   resources :users, except: [:new, :edit]
-  resources :events
+  resources :events, :rsvps
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
