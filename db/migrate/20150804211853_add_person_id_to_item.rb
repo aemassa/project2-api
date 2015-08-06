c15c9fb1f674d28fdde451209f18a429be3b159a
aemassa <annaemassa@gmail.com>
2015-08-06 11:33:44 -0400

    Delete migrations referring to the "items" table.
    Part of the process of removing the "items" table and replacing it with an "rsvps" table.

--- db/migrate/20150804211853_add_person_id_to_item.rb
+++
@@ -1,5 +0,0 @@
-class AddPersonIdToItem < ActiveRecord::Migration
-  def change
-    add_reference :items, :person, index: true, foreign_key: true
-  end
-end
--- db/migrate/20150804211948_add_event_id_to_item.rb
+++
@@ -1,5 +0,0 @@
-class AddEventIdToItem < ActiveRecord::Migration
-  def change
-    add_reference :items, :event, index: true, foreign_key: true
-  end
-end
